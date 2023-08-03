5e52f0e4ea63208e58cfea5f7791060aa473a3fa
skyum1 <rjftkd126@gmail.com>
2023-08-03 21:18:20 +0900

    Test #7

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,30 +1,20 @@
+import {useState} from "react";
 
 export default function Hello() {
-    function showName() {
-        console.log("Mike");
+    //let name = "Mike";
+    const [name, setName] = useState('Mike');
+    function changeName() {
+        const newName = name === "Mike" ? "Jane" : "Mike";
+        console.log(name);
+        //document.getElementById("name").innerText = name;
+        setName(newName);
     }
-
-    function showAge(age) {
-        console.log(age);
-    }
-
-    function showText(txt) {
-
-        console.log(txt);
-    }
-
     return ( 
         <div>
             <h1>Hello</h1>
-            <button onClick={showName}>Show name</button>
-            <button onClick={() => {
-                showAge(10);
-            }}>Show age</button>
-            <p></p>
-            <input type="text" onChange={(e)=> {
-               const txt = e.target.value;
-               showText(txt);
-            }}/>
+            <h2 id="name">{name}</h2>
+            <button onClick={changeName}>Change</button>
+            
         </div>
     );
 }
