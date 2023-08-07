7912cb0513d4c37b979f630c73f2893064dec64f
skyum1 <rjftkd126@gmail.com>
2023-08-07 21:54:26 +0900

    Test #12 useEffect in  DayList.js

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,15 +1,39 @@
 import { Link } from "react-router-dom";
-import dummy from "../db/data.json";
+import { useEffect, useState } from "react";
 
 export default function DayList() {
+    const [days,setDays] = useState([]);
+    const [count,setCount] = useState(0);
+
+    function onclick() {
+        setCount(count + 1);
+    }
+
+    function onclick2() {
+        setDays([
+            ...days,
+            {
+                id : Math.random(),
+                day:1,
+            }
+        ])
+    }
+    useEffect(() => {
+        console.log("Count change");
+    }, []);
+
     return (
+        <>
         <ul className="list_day">
             {
-                dummy.days.map(day => (
+                days.map(day => (
                     <li key={day.id}>
                         <Link to={`/day/${day.day}`}>Day {day.day}</Link>
                     </li> 
             ))}
         </ul>
+        <button onClick={onclick}>{count}</button>
+        <button onClick={onclick2}>daychange</button>
+        </>
     );
 }
