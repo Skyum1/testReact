e4f1bded8f2b534e0f5345c236d0a9c661950489
skyum1 <rjftkd126@gmail.com>
2023-08-09 21:51:01 +0900

    Test #14 hooks

--- src/component/Day.js
+++ src/component/Day.js
@@ -2,23 +2,11 @@ import dummy from "../db/data.json";
 import { useParams } from "react-router-dom";
 import Word from "./Word";
 import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
 
 export default function Day() {
     const {day} = useParams();
-    //const wordList = dummy.words.filter(word => word.day === Number(day));
-    const [words, setWords] = useState([]);
-    //console.log(dummy);
-    //console.log(day);
-
-  useEffect(() =>{
-    fetch(`http://localhost:3001/words?day=${day}`)
-        .then(res => {
-            return res.json()
-        })
-        .then(data => {
-            setWords(data);
-        })
-    }, [day]);
+    const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
   return (
     <>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,29 +1,16 @@
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
 
 export default function DayList() {
-    const [days,setDays] = useState([]);
+  const days = useFetch("http://localhost:3001/days");
 
-    useEffect(() => {
-        fetch("http://localhost:3001/days")
-        .then(res => {
-            return res.json()
-        })
-        .then(data => {
-            setDays(data);
-        })
-    }, []);
-
-    return (
-        <>
-        <ul className="list_day">
-            {
-                days.map(day => (
-                    <li key={day.id}>
-                        <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-                    </li> 
-            ))}
-        </ul>
-        </>
-    );
+  return (
+    <ul className="list_day">
+      {days.map(day => (
+        <li key={day.id}>
+          <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
